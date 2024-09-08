46a67730005b422cc678c8fe9896de804ffbb519
shemsudi <nuryeshemsedin@gmail.com>
2024-09-08 22:33:39 +0300

    add header and navigation of location page

--- frontend/src/pages/hostingSteps/locationPage.jsx
+++ frontend/src/pages/hostingSteps/locationPage.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import HostHeader from "./hostHeader";
+import ProgressBar from "./progressBar";
+import FooterNavigation from "./footerNavigation";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const LocationPage = () => {
+  const host = useSelector((state) => state.host.host);
+  const navigate = useNavigate();
+  const onNext = () => {
+    navigate(`/became-a-host/${host.uuid}/family`);
+  };
+  const onBack = () => {
+    navigate(`/became-a-host/${host.uuid}/privacyType`);
+  };
   return (
-    <div>
-      <h1>location Page</h1>
+    <div className="h-screen flex flex-col">
+      <HostHeader />
+      <div className="flex-1"></div>
+      <ProgressBar step={1} pos={3} />
+      <FooterNavigation onBack={onBack} onNext={onNext} />
     </div>
   );
 };
